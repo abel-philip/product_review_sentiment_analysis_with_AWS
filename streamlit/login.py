2e95c85d6e68ab4702e89657c29719b7d24b9709
abel-philip <63131791+abel-philip@users.noreply.github.com>
2021-11-01 12:07:41 -0400

    Update login.py

--- streamlit/login.py
+++ streamlit/login.py
@@ -20,8 +20,8 @@ def app():
                 message = "Authenticated Successfully"
                 st.balloons()
             else:
-                variable_store.status = True
-                message = "Authenticated Successfully"
+                variable_store.status = False
+                message = "Authentication Failed"
             st.header(message)
     if radio_selection=='Create New':
         st.write("Creating New user")
